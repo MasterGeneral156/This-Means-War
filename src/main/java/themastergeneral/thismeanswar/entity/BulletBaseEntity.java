97aa1400daf10c51ddcab570c7b084c5e7f33af7
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2020-12-22 20:29:27 -0500

    Possibly add icons to projectils?

--- src/main/java/themastergeneral/thismeanswar/entity/BulletBaseEntity.java
+++ src/main/java/themastergeneral/thismeanswar/entity/BulletBaseEntity.java
@@ -6,11 +6,16 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ProjectileItemEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.particles.IParticleData;
+import net.minecraft.particles.ItemParticleData;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.EntityRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import themastergeneral.thismeanswar.ModConstant;
 
 public class BulletBaseEntity extends ProjectileItemEntity 
@@ -31,6 +36,12 @@ public class BulletBaseEntity extends ProjectileItemEntity
 	{
 	      super(TMWEntities.BULLET_ENTITY, x, y, z, worldIn);
 	      bulletDmg = damage;
+	}
+	
+	@OnlyIn(Dist.CLIENT)
+	private IParticleData makeParticle() 
+	{
+	  return (IParticleData)new ItemParticleData(ParticleTypes.ITEM, new ItemStack(bulletItm));
 	}
 
 	public BulletBaseEntity(World worldIn, LivingEntity throwerIn, float damage, Item bullet) {
