6df9a680ec3f5e14b155bbeaf6e3dc79dde83e87
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2021-11-21 17:52:53 -0500

    Big oof

--- src/main/java/themastergeneral/thismeanswar/block/TMWBlocks.java
+++ src/main/java/themastergeneral/thismeanswar/block/TMWBlocks.java
@@ -1,6 +1,6 @@
 package themastergeneral.thismeanswar.block;
 
 public class TMWBlocks {
-	public static TMWBlockBase lead_ore = new TMWBlockOre(3);
-	public static TMWBlockBase brass_ore = new TMWBlockOre(2);
+	public static TMWBlockOre lead_ore = new TMWBlockOre(3);
+	public static TMWBlockOre brass_ore = new TMWBlockOre(2);
 }
