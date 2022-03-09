7f18ea7c2d79f88509372ae8079f11d97605a863
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2022-03-08 21:23:57 -0500

    Clean this up.

--- src/main/java/themastergeneral/thismeanswar/registry/TMWEntityRegistry.java
+++ src/main/java/themastergeneral/thismeanswar/registry/TMWEntityRegistry.java
@@ -10,7 +10,7 @@ import themastergeneral.thismeanswar.entity.TMWEntities;
 public class TMWEntityRegistry {
 	public static final DeferredRegister<EntityType<?>> ENTITES = DeferredRegister.create(ForgeRegistries.ENTITIES, TMWMain.MODID);
 	
-	public static final RegistryObject<EntityType<?>> bullet_entity = TMWEntityRegistry.ENTITES.register("bullet_entity", () -> TMWEntities.BULLET_ENTITY);
-	public static final RegistryObject<EntityType<?>> contact_grenade_entity = TMWEntityRegistry.ENTITES.register("contact_grenade_entity", () -> TMWEntities.CONTACT_GRENADE_ENTITY);
-	public static final RegistryObject<EntityType<?>> rocket_entity = TMWEntityRegistry.ENTITES.register("rocket_entity", () -> TMWEntities.ROCKET_ENTITY);
+	public static final RegistryObject<EntityType<?>> bullet_entity = ENTITES.register("bullet_entity", () -> TMWEntities.BULLET_ENTITY);
+	public static final RegistryObject<EntityType<?>> contact_grenade_entity = ENTITES.register("contact_grenade_entity", () -> TMWEntities.CONTACT_GRENADE_ENTITY);
+	public static final RegistryObject<EntityType<?>> rocket_entity = ENTITES.register("rocket_entity", () -> TMWEntities.ROCKET_ENTITY);
 }
