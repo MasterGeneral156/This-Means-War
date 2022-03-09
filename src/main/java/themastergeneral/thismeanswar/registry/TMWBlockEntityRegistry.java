121bf33daacfd119344b48a69969475916257929
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2022-03-08 21:20:09 -0500

    Update TMWBlockEntityRegistry.java

--- src/main/java/themastergeneral/thismeanswar/registry/TMWBlockEntityRegistry.java
+++ src/main/java/themastergeneral/thismeanswar/registry/TMWBlockEntityRegistry.java
@@ -5,6 +5,6 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import themastergeneral.thismeanswar.TMWMain;
 
-public class TMWTileEntityRegistry {
+public class TMWBlockEntityRegistry {
 	public static final DeferredRegister<BlockEntityType<?>> TILES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, TMWMain.MODID);
 }
