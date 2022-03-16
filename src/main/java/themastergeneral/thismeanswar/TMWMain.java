14e7e66003dfa9a14df0bb28577d5e91a40cc445
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2022-03-16 16:48:29 -0400

    Renderer for ammo box... doesn't work?

--- src/main/java/themastergeneral/thismeanswar/TMWMain.java
+++ src/main/java/themastergeneral/thismeanswar/TMWMain.java
@@ -3,11 +3,15 @@ package themastergeneral.thismeanswar;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import themastergeneral.thismeanswar.block.TMWBlocks;
 import themastergeneral.thismeanswar.registry.TMWBlockEntityRegistry;
 import themastergeneral.thismeanswar.registry.TMWBlockRegistry;
 import themastergeneral.thismeanswar.registry.TMWEntityRegistry;
@@ -23,6 +27,7 @@ public class TMWMain
 
     public TMWMain() {
     	FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    	FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
     	
     	MinecraftForge.EVENT_BUS.register(this);
         TMWItemRegistry.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
@@ -35,5 +40,11 @@ public class TMWMain
     {
         LOGGER.info("This Means War, in active development.");
         //OreGenHandler.registerOres();
+    }
+    
+    private void clientSetup(final FMLClientSetupEvent event)
+    {
+    	LOGGER.info("Loading client-side Block Render layers.");
+    	ItemBlockRenderTypes.setRenderLayer(TMWBlocks.ammo_box, RenderType.translucent());
     }
 }
