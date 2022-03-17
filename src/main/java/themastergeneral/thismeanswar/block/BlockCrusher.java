449a0df92ef65a966f589e43dc6b03eda7b097f4
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2022-03-16 21:58:25 -0400

    Create BlockCrusher.java

---
+++ src/main/java/themastergeneral/thismeanswar/block/BlockCrusher.java
@@ -0,0 +1,51 @@
+package themastergeneral.thismeanswar.block;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.BlockHitResult;
+
+public abstract class BlockCrusher extends BaseEntityBlock {
+	public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+	   public static final BooleanProperty LIT = BlockStateProperties.LIT;
+	   
+	   protected BlockCrusher(BlockBehaviour.Properties p_48687_) {
+		      super(p_48687_);
+		      this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(LIT, Boolean.valueOf(false)));
+		   }
+
+	@Nullable
+	@Override
+	public BlockEntity newBlockEntity(BlockPos p_153215_, BlockState p_153216_) {
+		return null;
+	}
+	
+	public InteractionResult use(BlockState p_48706_, Level p_48707_, BlockPos p_48708_, Player p_48709_, InteractionHand p_48710_, BlockHitResult p_48711_) {
+	      if (p_48707_.isClientSide) {
+	         return InteractionResult.SUCCESS;
+	      } else {
+	         this.openContainer(p_48707_, p_48708_, p_48709_);
+	         return InteractionResult.CONSUME;
+	      }
+	   }
+	protected abstract void openContainer(Level p_48690_, BlockPos p_48691_, Player p_48692_);
+
+	public BlockState getStateForPlacement(BlockPlaceContext p_48689_) {
+      return this.defaultBlockState().setValue(FACING, p_48689_.getHorizontalDirection().getOpposite());
+	}
+
+}
