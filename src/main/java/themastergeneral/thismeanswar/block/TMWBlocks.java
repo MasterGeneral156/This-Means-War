a8b6cc981fc8e5cd29dfdfc7872ee375b0e9280a
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2022-03-18 18:07:40 -0400

    Added block resistance to metal blocks.

--- src/main/java/themastergeneral/thismeanswar/block/TMWBlocks.java
+++ src/main/java/themastergeneral/thismeanswar/block/TMWBlocks.java
@@ -9,9 +9,9 @@ public class TMWBlocks {
 	public static BlockOre ore_lead = new BlockOre(3);
 	public static BlockOre ore_brass = new BlockOre(2);
 	
-	public static BlockBase block_lead = new BlockBase(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.METAL).sound(SoundType.METAL));
-	public static BlockBase block_steel = new BlockBase(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.METAL).sound(SoundType.METAL));
-	public static BlockBase block_brass = new BlockBase(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.METAL).sound(SoundType.METAL));
+	public static BlockBase block_lead = new BlockBase(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.METAL).sound(SoundType.METAL).strength(5.0F, 6.0F));
+	public static BlockBase block_steel = new BlockBase(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.METAL).sound(SoundType.METAL).strength(5.0F, 6.0F));
+	public static BlockBase block_brass = new BlockBase(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.METAL).sound(SoundType.METAL).strength(5.0F, 6.0F));
 	
 	public static BlockAmmoStorage ammo_box = new BlockAmmoStorage(512);
 	public static BlockAmmoStorage ammo_box_medium = new BlockAmmoStorage(1024);
