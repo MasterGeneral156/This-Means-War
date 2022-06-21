df13efaed0ff84dd2dd493219dda751fff4f01f1
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2022-06-20 22:52:44 -0400

    Update CrusherMenu.java

--- src/main/java/themastergeneral/thismeanswar/menu/CrusherMenu.java
+++ src/main/java/themastergeneral/thismeanswar/menu/CrusherMenu.java
@@ -1,5 +1,19 @@
 package themastergeneral.thismeanswar.menu;
 
-public class CrusherMenu {
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
+
+public class CrusherMenu extends AbstractContainerMenu {
+
+	protected CrusherMenu(MenuType<?> p_38851_, int p_38852_) {
+		super(p_38851_, p_38852_);
+	}
+
+	@Override
+	public boolean stillValid(Player p_38874_) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
