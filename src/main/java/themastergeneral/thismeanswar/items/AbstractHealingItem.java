21dd15134e221a9c0ea6bbf54b07986fff9eee4d
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2022-07-11 20:21:19 -0400

    Update AbstractHealingItem.java

--- src/main/java/themastergeneral/thismeanswar/items/AbstractHealingItem.java
+++ src/main/java/themastergeneral/thismeanswar/items/AbstractHealingItem.java
@@ -1,14 +1,23 @@
 package themastergeneral.thismeanswar.items;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import themastergeneral.thismeanswar.TMWMain;
 
 public class AbstractHealingItem extends AbstractModItem {
@@ -54,7 +63,15 @@ public class AbstractHealingItem extends AbstractModItem {
 	protected void doTreatmentTick(Player playerIn)
 	{
 		playerIn.getCooldowns().addCooldown(this, ticks);
-		playerIn.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, this.ticks, 25));
+		playerIn.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, this.ticks, 25, false, false));
+	}
+	
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void appendHoverText(ItemStack stack, @Nullable Level worldIn, List<Component> tooltip, TooltipFlag flagIn) 
+	{
+		if (this.health > 0)
+			tooltip.add(new TranslatableComponent("Instantly recovers " + Math.round(this.health) + " health."));
 	}
 
 }
