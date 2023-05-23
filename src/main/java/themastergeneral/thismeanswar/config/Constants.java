0800797cea3b8049f7d843ee5b0658ce4b6241ba
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2023-05-22 22:59:23 -0400

    Semi auto rebalance

--- src/main/java/themastergeneral/thismeanswar/config/Constants.java
+++ src/main/java/themastergeneral/thismeanswar/config/Constants.java
@@ -7,6 +7,7 @@ public class Constants
 	public static int fireRateBurst = 3;
 	
 	public static int cooldownThrowable = 15;
+	public static int cooldownSemiAuto = 25;
 	public static float sawnOffBonusSpread = 5.1356F;
 	public static float sawnOffBonusDamage = 2.682F;
 	public static float baseSpread = 1.4F;
@@ -52,55 +53,55 @@ public class Constants
 	public static float tmgCarbineSpread = 0.981F + baseSpread9mm;
 	
 	//AR15 (223)
-	public static int ar15FireRate223 = fireRateSemi;
+	public static int ar15FireRate223 = cooldownSemiAuto;
 	public static int ar15ReloadTime223 = 45;
 	public static float ar15Damage223 = 2.526F + baseDamage223;
 	public static float ar15Speed223 = 11.919F + baseSpeed223;
 	public static float ar15Spread223 = 1.384F + baseSpread223;
 	
 	//AR15 (556)
-	public static int ar15FireRate556 = fireRateSemi;
+	public static int ar15FireRate556 = cooldownSemiAuto;
 	public static int ar15ReloadTime556 = 45;
 	public static float ar15Damage556 = 5.7848F + baseDamage556;
 	public static float ar15Speed556 = 15.312F + baseSpeed556;
 	public static float ar15Spread556 = 1.625F + baseSpread556;
 	
 	//Bazooka
-	public static int bazookaFireRate = fireRateSemi;
+	public static int bazookaFireRate = cooldownSemiAuto;
 	public static int bazookaReloadTime = 35;
 	public static float bazookaDamage = 5.75F;
 	public static float bazookaSpeed = 50.53F;
 	
 	//Beretta 92
-	public static int beretta92FireRate = fireRateSemi;
+	public static int beretta92FireRate = cooldownSemiAuto;
 	public static int beretta92ReloadTime = 50;
 	public static float beretta92Damage = 6.1757F + baseDamage9mm;
 	public static float beretta92Speed = 7.71243F + baseSpeed9mm;
 	public static float beretta92Spread = 0.99F + baseSpread9mm;
 	
 	//Glock 26
-	public static int glock26FireRate = fireRateSemi;
+	public static int glock26FireRate = cooldownSemiAuto;
 	public static int glock26ReloadTime = 40;
 	public static float glock26Damage = 5.77687F + baseDamage9mm;
 	public static float glock26Speed = 7.97243F + baseSpeed9mm;
 	public static float glock26Spread = 1.028F + baseSpread9mm;
 	
 	//M1911
-	public static int m1911FireRate = fireRateSemi;
+	public static int m1911FireRate = cooldownSemiAuto;
 	public static int m1911ReloadTime = 54;
 	public static float m1911Damage = 7.1245F + baseDamage45;
 	public static float m1911Speed = 14.124F + baseSpeed45;
 	public static float m1911Spread = 1.1F + baseSpread45;
 	
 	//M870
-	public static int m870FireRate = fireRateSemi;
+	public static int m870FireRate = cooldownSemiAuto;
 	public static int m870ReloadTime = 8;
 	public static float m870Damage = 10.3231F + baseDamage12g;
 	public static float m870Speed = 52.65243F + baseSpeed12g;
 	public static float m870Spread = 1.13F + baseSpread12g;
 	
 	//Double Barrel
-	public static int doubleBarrelFireRate = fireRateSemi;
+	public static int doubleBarrelFireRate = cooldownSemiAuto;
 	public static int doubleBarrelReloadTime = 8;
 	public static float doubleBarrelDamage = 11.479F + baseDamage12g;
 	public static float doubleBarrelSpeed = 56.36843F + baseSpeed12g;
