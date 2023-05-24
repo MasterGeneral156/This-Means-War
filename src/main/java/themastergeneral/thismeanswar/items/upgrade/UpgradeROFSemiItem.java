840dab3e0a8c87ea63ea87a2025a0d5f159d897e
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2023-05-24 19:03:57 -0400

    Can now disable upgrades via tags

--- src/main/java/themastergeneral/thismeanswar/items/upgrade/UpgradeROFSemiItem.java
+++ src/main/java/themastergeneral/thismeanswar/items/upgrade/UpgradeROFSemiItem.java
@@ -11,12 +11,16 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.tags.ITagManager;
 import themastergeneral.thismeanswar.config.Constants;
+import themastergeneral.thismeanswar.config.TMWTags;
 import themastergeneral.thismeanswar.items.AbstractGunItem;
 import themastergeneral.thismeanswar.items.BasicItem;
 
@@ -26,25 +30,33 @@ public class UpgradeROFSemiItem extends BasicItem {
 	public InteractionResultHolder<ItemStack> use(Level worldIn, Player playerIn, InteractionHand handIn) 
 	{
 		ItemStack offHandStack = playerIn.getOffhandItem();
-		if (offHandStack.getItem() instanceof AbstractGunItem)
+		ITagManager<Item> tagManager = ForgeRegistries.ITEMS.tags();
+		if ((!tagManager.getTag(TMWTags.disableAllUpgrade).contains(offHandStack.getItem())) && 
+				(!tagManager.getTag(TMWTags.disableSemiAutoUpgrade).contains(offHandStack.getItem())))
 		{
-			AbstractGunItem offhand = (AbstractGunItem) playerIn.getOffhandItem().getItem();
-			if (offhand.shotTime > Constants.fireRateAuto)
+			if (offHandStack.getItem() instanceof AbstractGunItem)
 			{
-				//fail
-				playerIn.displayClientMessage(ModUtils.displayTranslation("thismeanswar.upgrade_nocompat"), true);
-				return InteractionResultHolder.fail(playerIn.getMainHandItem());
+				AbstractGunItem offhand = (AbstractGunItem) playerIn.getOffhandItem().getItem();
+				if (offhand.shotTime > Constants.fireRateAuto)
+				{
+					//fail
+					playerIn.displayClientMessage(ModUtils.displayTranslation("thismeanswar.upgrade_nocompat"), true);
+					return InteractionResultHolder.fail(playerIn.getMainHandItem());
+				}
+				else
+				{
+					offhand.setGunROF(playerIn.getItemInHand(InteractionHand.OFF_HAND), Constants.fireRateSemi);
+					playerIn.getMainHandItem().shrink(1);
+					playerIn.displayClientMessage(ModUtils.displayTranslation("thismeanswar.upgrade_rof_semi"), true);
+					return InteractionResultHolder.pass(playerIn.getMainHandItem());
+				}
 			}
 			else
-			{
-				offhand.setGunROF(playerIn.getItemInHand(InteractionHand.OFF_HAND), Constants.fireRateSemi);
-				playerIn.getMainHandItem().shrink(1);
-				playerIn.displayClientMessage(ModUtils.displayTranslation("thismeanswar.upgrade_rof_semi"), true);
-				return InteractionResultHolder.pass(playerIn.getMainHandItem());
-			}
+				return InteractionResultHolder.fail(playerIn.getMainHandItem());
 		}
 		else
 		{
+			playerIn.displayClientMessage(ModUtils.displayTranslation("thismeanswar.upgrade_fail_disabled"), true);
 			return InteractionResultHolder.fail(playerIn.getMainHandItem());
 		}
 	}
