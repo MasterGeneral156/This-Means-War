87035df35f83a3f918b6a94a0c513b2660309f04
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2023-06-13 21:49:28 -0400

    Reorganize tab

--- src/main/java/themastergeneral/thismeanswar/TMWTabs.java
+++ src/main/java/themastergeneral/thismeanswar/TMWTabs.java
@@ -3,6 +3,7 @@ package themastergeneral.thismeanswar;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 import themastergeneral.thismeanswar.items.TMWItems;
@@ -13,17 +14,20 @@ public class TMWTabs {
 	public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TMWMain.MODID);
 	
 	public static final RegistryObject<CreativeModeTab> MAIN_TAB = CREATIVE_MODE_TABS.register("tmw_main", () -> CreativeModeTab.builder()
+			.withTabsBefore(CreativeModeTabs.COMBAT)
             .icon(() -> TMWItems.ammo_box.getDefaultInstance())
             .title(Component.translatable("itemGroup.thismeanswar"))
             .build());
 	
-	public static final RegistryObject<CreativeModeTab> GUN_TAB = CREATIVE_MODE_TABS.register("tmw_guns", () -> CreativeModeTab.builder()
-            .icon(() -> TMWPistols.m1911.getDefaultInstance())
-            .title(Component.translatable("itemGroup.thismeanswar.guns"))
+	public static final RegistryObject<CreativeModeTab> AMMO_TAB = CREATIVE_MODE_TABS.register("tmw_ammo", () -> CreativeModeTab.builder()
+			.icon(() -> TMWItems.round_40mm.getDefaultInstance())
+            .title(Component.translatable("itemGroup.thismeanswar.ammo"))
             .build());
 	
-	public static final RegistryObject<CreativeModeTab> AMMO_TAB = CREATIVE_MODE_TABS.register("tmw_ammo", () -> CreativeModeTab.builder()
-            .icon(() -> TMWItems.round_40mm.getDefaultInstance())
-            .title(Component.translatable("itemGroup.thismeanswar.ammo"))
+	public static final RegistryObject<CreativeModeTab> GUN_TAB = CREATIVE_MODE_TABS.register("tmw_guns", () -> CreativeModeTab.builder()
+            .withTabsBefore(MAIN_TAB.getKey())
+            .withTabsAfter(AMMO_TAB.getKey())
+			.icon(() -> TMWPistols.m1911.getDefaultInstance())
+            .title(Component.translatable("itemGroup.thismeanswar.guns"))
             .build());
 }
