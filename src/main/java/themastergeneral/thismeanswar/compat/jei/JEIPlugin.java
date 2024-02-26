ab271fbf44ad6c5bda656bf284bde926070ad6a0
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2024-02-26 15:07:40 -0500

    Add JEI descriptions

--- src/main/java/themastergeneral/thismeanswar/compat/jei/JEIPlugin.java
+++ src/main/java/themastergeneral/thismeanswar/compat/jei/JEIPlugin.java
@@ -2,6 +2,8 @@ package themastergeneral.thismeanswar.compat.jei;
 
 import javax.annotation.Nonnull;
 
+import com.themastergeneral.ctdcore.helpers.ModUtils;
+
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.helpers.IGuiHelper;
@@ -13,6 +15,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import themastergeneral.thismeanswar.TMWMain;
 import themastergeneral.thismeanswar.block.TMWBlocks;
+import themastergeneral.thismeanswar.items.TMWItems;
 import themastergeneral.thismeanswar.registry.TMWRecipeTypeRegistration;
 @JeiPlugin
 public class JEIPlugin implements IModPlugin {
@@ -36,6 +39,32 @@ public class JEIPlugin implements IModPlugin {
   public void registerRecipes(@Nonnull final IRecipeRegistration registration) 
 	{
 		TMWMain.LOGGER.info("Adding recipes to JEI!!");
+		registration.addItemStackInfo(new ItemStack(TMWItems.ammo_box), ModUtils.displayTranslation("thismeanswar.jei.ammo_box_info"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.ammo_box_medium), ModUtils.displayTranslation("thismeanswar.jei.ammo_box_info"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.ammo_box_large), ModUtils.displayTranslation("thismeanswar.jei.ammo_box_info"));
+		
+		registration.addItemStackInfo(new ItemStack(TMWItems.medic_box), ModUtils.displayTranslation("thismeanswar.jei.medic_box_info"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.medic_box_medium), ModUtils.displayTranslation("thismeanswar.jei.medic_box_info"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.medic_box_large), ModUtils.displayTranslation("thismeanswar.jei.medic_box_info"));
+		
+		registration.addItemStackInfo(new ItemStack(TMWItems.base_upgrade), ModUtils.displayTranslation("thismeanswar.jei.upgrade_info"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.bullet_upgrade_ap), ModUtils.displayTranslation("thismeanswar.jei.upgrade_info"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.bullet_upgrade_ap), ModUtils.displayTranslation("thismeanswar.jei.upgrade_info_ap"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.bullet_upgrade_fire), ModUtils.displayTranslation("thismeanswar.jei.upgrade_info"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.bullet_upgrade_fire), ModUtils.displayTranslation("thismeanswar.jei.upgrade_info_fire"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.bullet_upgrade_inert), ModUtils.displayTranslation("thismeanswar.jei.upgrade_info"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.bullet_upgrade_inert), ModUtils.displayTranslation("thismeanswar.jei.upgrade_info_inert"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.bullet_upgrade_normal), ModUtils.displayTranslation("thismeanswar.jei.upgrade_info"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.bullet_upgrade_normal), ModUtils.displayTranslation("thismeanswar.jei.upgrade_info_normal"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.bullet_upgrade_tracer), ModUtils.displayTranslation("thismeanswar.jei.upgrade_info"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.bullet_upgrade_tracer), ModUtils.displayTranslation("thismeanswar.jei.upgrade_info_tracer"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.gun_rof_upgrade), ModUtils.displayTranslation("thismeanswar.jei.upgrade_info"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.gun_rof_upgrade), ModUtils.displayTranslation("thismeanswar.jei.upgrade_info_rofup"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.gun_rof_downgrade), ModUtils.displayTranslation("thismeanswar.jei.upgrade_info"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.gun_rof_upgrade), ModUtils.displayTranslation("thismeanswar.jei.upgrade_info_rofdown"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.mag_capacity_upgrade), ModUtils.displayTranslation("thismeanswar.jei.upgrade_info"));
+		registration.addItemStackInfo(new ItemStack(TMWItems.mag_capacity_upgrade), ModUtils.displayTranslation("thismeanswar.jei.upgrade_magcap"));
+		
 		registration.addRecipes(RecipeTypes.BULLET_FOUNDARY, Minecraft.getInstance().level.getRecipeManager().getAllRecipesFor(TMWRecipeTypeRegistration.FOUNDARY_TYPE.get()));
 	}
 	
