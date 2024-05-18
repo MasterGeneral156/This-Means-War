cedaf46e0574f93aacf795ea427905c9088931b7
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2024-05-17 22:27:50 -0400

    Update SmokeThrowableItem.java

--- src/main/java/themastergeneral/thismeanswar/items/SmokeThrowableItem.java
+++ src/main/java/themastergeneral/thismeanswar/items/SmokeThrowableItem.java
@@ -8,7 +8,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import themastergeneral.thismeanswar.config.Constants;
-import themastergeneral.thismeanswar.entity.ContactGrenadeEntity;
 import themastergeneral.thismeanswar.entity.SmokeThrowableEntity;
 import themastergeneral.thismeanswar.items.interfaces.AbstractModItem;
 
