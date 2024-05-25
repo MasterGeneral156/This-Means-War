7a787cadc5bf13eea9fb1ebcf2c7b35cdcd84dd7
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2024-05-25 05:01:52 -0400

    Give trades to villagers who have professions

--- src/main/java/themastergeneral/thismeanswar/TMWEvents.java
+++ src/main/java/themastergeneral/thismeanswar/TMWEvents.java
@@ -3,6 +3,7 @@ package themastergeneral.thismeanswar;
 import java.util.Random;
 
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import net.minecraft.world.phys.AABB;
 import net.minecraftforge.event.TickEvent;
@@ -32,36 +33,39 @@ public class TMWEvents {
     public static void onVillagerTradesSetup(VillagerTradesEvent event) 
 	{
 		TMWMain.debugLogger("Giving trades to villagers...");
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_lead, 16, TMWItems.round_12g, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_lead, 16, TMWItems.round_223, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_lead, 16, TMWItems.round_38spec, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_lead, 16, TMWItems.round_45, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_lead, 16, TMWItems.round_556, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_lead, 16, TMWItems.round_762, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_lead, 16, TMWItems.round_9mm, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
-		
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_lead, 8, TMWItems.bullet_cast_12g, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_lead, 8, TMWItems.bullet_cast_223, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_lead, 8, TMWItems.bullet_cast_38spec, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_lead, 8, TMWItems.bullet_cast_45, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_lead, 8, TMWItems.bullet_cast_556, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_lead, 8, TMWItems.bullet_cast_762, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_lead, 8, TMWItems.bullet_cast_9mm, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
-		
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_brass, 16, TMWItems.casing_12g, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_brass, 16, TMWItems.casing_223, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_brass, 16, TMWItems.casing_38spec, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_brass, 16, TMWItems.casing_45, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_brass, 16, TMWItems.casing_556, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_brass, 16, TMWItems.casing_762, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_brass, 16, TMWItems.casing_9mm, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
-		
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_brass, 8, TMWItems.casing_cast_12g, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_brass, 8, TMWItems.casing_cast_223, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_brass, 8, TMWItems.casing_cast_38spec, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_brass, 8, TMWItems.casing_cast_45, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_brass, 8, TMWItems.casing_cast_556, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_brass, 8, TMWItems.casing_cast_762, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
-		event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_brass, 8, TMWItems.casing_cast_9mm, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
+		if (event.getType() != VillagerProfession.NONE)
+		{
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_lead, 16, TMWItems.round_12g, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_lead, 16, TMWItems.round_223, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_lead, 16, TMWItems.round_38spec, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_lead, 16, TMWItems.round_45, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_lead, 16, TMWItems.round_556, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_lead, 16, TMWItems.round_762, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_lead, 16, TMWItems.round_9mm, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
+			
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_lead, 8, TMWItems.bullet_cast_12g, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_lead, 8, TMWItems.bullet_cast_223, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_lead, 8, TMWItems.bullet_cast_38spec, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_lead, 8, TMWItems.bullet_cast_45, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_lead, 8, TMWItems.bullet_cast_556, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_lead, 8, TMWItems.bullet_cast_762, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_lead, 8, TMWItems.bullet_cast_9mm, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
+			
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_brass, 16, TMWItems.casing_12g, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_brass, 16, TMWItems.casing_223, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_brass, 16, TMWItems.casing_38spec, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_brass, 16, TMWItems.casing_45, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_brass, 16, TMWItems.casing_556, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_brass, 16, TMWItems.casing_762, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.nugget_brass, 16, TMWItems.casing_9mm, 16, 8, 0, new Random().nextFloat(0.5F, 2.0F)));
+			
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_brass, 8, TMWItems.casing_cast_12g, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_brass, 8, TMWItems.casing_cast_223, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_brass, 8, TMWItems.casing_cast_38spec, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_brass, 8, TMWItems.casing_cast_45, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_brass, 8, TMWItems.casing_cast_556, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_brass, 8, TMWItems.casing_cast_762, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
+			event.getTrades().get(1).add(new TMWTrades(TMWItems.ingot_brass, 8, TMWItems.casing_cast_9mm, 1, 4, 0, new Random().nextFloat(0.5F, 2.0F)));
+		}
 	}
 }
