0901c229d3c1810b89ace069bb49511b812c5288
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2024-05-29 22:09:14 -0400

    Show durability on durability items. (Saw,Hammer,Casts,etc)

--- src/main/java/themastergeneral/thismeanswar/items/DurabilityItem.java
+++ src/main/java/themastergeneral/thismeanswar/items/DurabilityItem.java
@@ -1,12 +1,33 @@
 package themastergeneral.thismeanswar.items;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import com.themastergeneral.ctdcore.helpers.ModUtils;
 import com.themastergeneral.ctdcore.item.CTDDurabilityItem;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class DurabilityItem extends CTDDurabilityItem {
 
+	protected int durability;
+	
 	public DurabilityItem(int durability) {
 		super(new Properties().stacksTo(1), durability);
+		this.durability = durability;
+	}
+	
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void appendHoverText(ItemStack stack, @Nullable Level worldIn, List<Component> tooltip, TooltipFlag flagIn) 
+	{
+		if (stack.isDamageableItem())
+			tooltip.add(ModUtils.displayString("Durability: " + this.getMaxDamage(stack)));
 	}
 }
