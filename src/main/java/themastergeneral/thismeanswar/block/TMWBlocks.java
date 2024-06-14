ab92314c87d13f5d84db7ee25babc1d96b9fe7d1
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2024-06-14 16:59:55 -0400

    More rounds in ammo blocks.

--- src/main/java/themastergeneral/thismeanswar/block/TMWBlocks.java
+++ src/main/java/themastergeneral/thismeanswar/block/TMWBlocks.java
@@ -13,9 +13,9 @@ public class TMWBlocks {
 	public static RedTeamBlock red_chiseled_bricks = new RedTeamBlock();
 	public static BlueTeamBlock blue_chiseled_bricks = new BlueTeamBlock();
 	
-	public static BlockAmmoStorage ammo_box = new BlockAmmoStorage(512);
-	public static BlockAmmoStorage ammo_box_medium = new BlockAmmoStorage(1024);
-	public static BlockAmmoStorage ammo_box_large = new BlockAmmoStorage(2048);
+	public static BlockAmmoStorage ammo_box = new BlockAmmoStorage(1024);
+	public static BlockAmmoStorage ammo_box_medium = new BlockAmmoStorage(2048);
+	public static BlockAmmoStorage ammo_box_large = new BlockAmmoStorage(4096);
 	
 	public static BlockMedicBox medic_box = new BlockMedicBox(1024F);
 	public static BlockMedicBox medic_box_medium = new BlockMedicBox(2048F);
