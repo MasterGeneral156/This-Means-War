91d61f815697383b37859fd4ed84a1145f3237ac
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2024-06-22 21:11:03 -0400

    Update TMWUtils.java

--- src/main/java/themastergeneral/thismeanswar/TMWUtils.java
+++ src/main/java/themastergeneral/thismeanswar/TMWUtils.java
@@ -1,5 +1,10 @@
 package themastergeneral.thismeanswar;
 
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import themastergeneral.thismeanswar.items.NuBulletBoxItem;
+import themastergeneral.thismeanswar.items.NuMagazineItem;
+
 public class TMWUtils {
 	
 	public static float makeFloatReadable(float value)
@@ -10,6 +15,12 @@ public class TMWUtils {
 	public static double makeDoubleReadable(double value)
 	{
 		return Math.round(value * 100D) / 100D;
+	}
+	
+	public static boolean isMagButNotBox(ItemStack stack)
+	{
+		Item item = stack.getItem();
+		return ((item instanceof NuMagazineItem) && (!(item instanceof NuBulletBoxItem)));
 	}
 
 }
