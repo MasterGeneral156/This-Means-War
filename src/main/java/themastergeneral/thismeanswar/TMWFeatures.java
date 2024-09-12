63d1293fe6ffbf695f7808a3d4a1f76fea3fa031
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2024-09-11 21:32:53 -0400

    WIP ore gen

--- src/main/java/themastergeneral/thismeanswar/TMWFeatures.java
+++ src/main/java/themastergeneral/thismeanswar/TMWFeatures.java
@@ -13,20 +13,18 @@ import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
 import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
 import net.minecraftforge.registries.ForgeRegistries;
 import themastergeneral.thismeanswar.block.TMWBlocks;
 
 public class TMWFeatures {
-	//public static final ResourceKey<ConfiguredFeature<?, ?>> CUSTOM_ORE_KEY = ResourceKey.create((ResourceKey<? extends Registry<T>>) ForgeRegistries.FEATURES, new ResourceLocation(TMWMain.MODID, "custom_ore"));
-	public static void bootstrap(BootstapContext<ConfiguredFeature<?, ?>> context) {
-	
-		List<OreConfiguration.TargetBlockState> OVERWORLD_CUSTOM_ORES = List.of(
-	            OreConfiguration.target(new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES), TMWBlocks.ore_brass.defaultBlockState())
-	    );
-		
-		ConfiguredFeature<?, ?> customOreFeature = new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(OVERWORLD_CUSTOM_ORES, 9));
-		//context.register(CUSTOM_ORE_KEY, customOreFeature);
+
+		public static final ConfiguredFeature<?, ?> ORE_BRASS = Feature.ORE
+				.configuredCodec(new OreConfiguration(
+						OreConfiguration.target(RuleTest),
+						TMWBlocks.ore_brass.defaultBlockState(),
+						9));
 	} 
 
 }
